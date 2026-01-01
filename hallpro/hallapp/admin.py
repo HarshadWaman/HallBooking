072516060a2d15661fb16d3dafcf5e262c734732
HarshadWaman <156565915+HarshadWaman@users.noreply.github.com>
2026-01-01 14:36:16 +0530

    end

--- hallpro/hallapp/admin.py
+++ hallpro/hallapp/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 #harshad waman
 from .models import Hall, Room, Booking 
-# Importing the models to register them in the 
+# Importing the models to register them in
