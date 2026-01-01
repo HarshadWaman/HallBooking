d43126e7a04786a4b83d6b04d98135645f77e449
HarshadWaman <156565915+HarshadWaman@users.noreply.github.com>
2026-01-01 20:47:11 +0530

    allowedhost

--- hallpro/hallpro/settings.py
+++ hallpro/hallpro/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-%q7m$sl-w#zp!d9e4(7zkt%i&$^tib76it!9)y!b^r^%+%_#m#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,7 +132,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
