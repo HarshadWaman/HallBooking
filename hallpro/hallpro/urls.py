7b94d5924f95c6d460f288503ca6c0a568c97eec
HarshadWaman <156565915+HarshadWaman@users.noreply.github.com>
2026-01-08 14:09:18 +0530

    codeupdate

--- hallpro/hallpro/urls.py
+++ hallpro/hallpro/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "Hallbooking"
+admin.site.site_title = "Hallbooking"
+admin.site.index_title = "Hallbooking"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hallapp.urls')),
