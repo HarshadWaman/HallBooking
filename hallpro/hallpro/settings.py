d85cc51085df17934046002e70c61033785fc175
HarshadWaman <156565915+HarshadWaman@users.noreply.github.com>
2026-01-13 10:46:34 +0530

    codeup1

--- hallpro/hallpro/settings.py
+++ hallpro/hallpro/settings.py
@@ -156,8 +156,8 @@ STATICFILES_DIRS = [
 # CSRF and Security Settings
 CSRF_TRUSTED_ORIGINS = ['https://hallbooking-wqcd.onrender.com',]
 
-#HTTPS Settings for Production (Render)
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+# HTTPS Settings for Production (Render)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
